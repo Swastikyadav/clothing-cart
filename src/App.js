5407b53f547b7d60513fbfcafe1b5e3675232835
Swastikyadav <swastikyadav.dev@gmail.com>
2019-11-01 19:58:46 +0530

    update app with redirections

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 import "./App.css";
@@ -27,9 +27,8 @@ class App extends React.Component {
             ...snapShot.data()
           });
         });
-      }
-      else {
-      setCurrentUser(userAuth);
+      } else {
+        setCurrentUser(userAuth);
       }
     });
   }
@@ -45,18 +44,27 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={Auth} />
+          <Route
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? <Redirect to="/" /> : <Auth />
+            }
+          />
         </Switch>
       </>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(App);
